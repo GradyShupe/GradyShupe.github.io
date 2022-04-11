2a272183fd5655675c31bb483a3125789c454e8b
GradyShupe <103457756+GradyShupe@users.noreply.github.com>
2022-04-11 15:52:28 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}';
+var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
@@ -10,16 +10,15 @@ var radarDisplayOptions = {
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
-  L.geoJSON(data, {
+L.geoJSON(data, {
   style: function(feature){
   var alertColor = 'orange';
   if (feature.properties.severity === 'Severe') alertColor = 'red';
   return { color: alertColor };
-  if (feature.properties.severity === 'minor') alertColor = 'green';
+    if (feature.properties.severity === 'minor') alertColor = 'green';
   return { color: alertColor };
 },
   onEachFeature: function(feature, layer) {
   layer.bindPopup(feature.properties.headline);
 }
 }).addTo(map);
-});
